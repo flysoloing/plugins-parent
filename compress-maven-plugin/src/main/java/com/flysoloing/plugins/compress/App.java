5aef9d1281dbde9d8ffbdce16c614d68ad070681
flysoloing <flysoloing@gmail.com>
2015-10-17 17:52:14 +0800

    plugins parent init

---
+++ compress-maven-plugin/src/main/java/com/flysoloing/plugins/compress/App.java
@@ -0,0 +1,13 @@
+package com.flysoloing.plugins.compress;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
